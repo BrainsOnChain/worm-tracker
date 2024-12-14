fa1ef02b8840699bdc9a36c94d39795817a45c8d
noahschumacher <noahschumacher14@gmail.com>
2024-12-14 15:40:29 -0500

    basic server

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/brainsonchain/worm-tracker
 
 go 1.22.5
+
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/go-chi/chi/v5 v5.1.0
+	go.uber.org/zap v1.27.0
+)
+
+require go.uber.org/multierr v1.10.0 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+
+	"github.com/brainsonchain/worm-tracker/server"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	// -------------------------------------------------------------------------
+	// Initialize the logger
+
+	logConfig := zap.NewDevelopmentConfig()
+	logConfig.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+
+	log, err := logConfig.Build()
+	if err != nil {
+		log.Sugar().Fatalf("error creating logger: %v", err)
+	}
+	zap.ReplaceGlobals(log)
+	log.Info("logger initialized")
+
+	// -------------------------------------------------------------------------
+	// Start the server
+	log.Info("starting server")
+
+	serverErr := make(chan error)
+
+	// Start the server
+	go func() {
+		server := server.NewServer(log, "8080")
+		serverErr <- server.Start()
+	}()
+
+	err = <-serverErr
+	if err != nil {
+		log.Sugar().Fatalf("error starting server: %v", err)
+	}
 }
